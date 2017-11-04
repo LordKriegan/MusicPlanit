7e10cb8ad5c29d2ed6ab0097990a882410062515
unknown <you@example.com>
2017-11-04 14:10:44 -0700

    Ticketmaster Js created

---
+++ assets/js/ticketmaster.js
@@ -0,0 +1,37 @@
+var city = "";
+
+
+window.onload = function() {
+    $("#searchBtn").on("click", function(event) {
+        event.preventDefault();
+        city = $("#searchString").val().trim();
+
+        var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?&apikey=BZAkAGm6c4G9IYugsrmGfucSP3F5PcSf&city=" + city;
+
+        $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+            .done(function(response) {
+                console.log(response);
+                for (i = 0; i < response._embedded.events.length; i++) {
+                    var eventImage = $("<img>");
+                    eventImage.attr("src", response._embedded.events[i].images[1].url);
+                    console.log(eventImage);
+
+                    var divContainer = $("<div>");
+                    divContainer.attr("id", response._embedded.events[i].id);
+                    divContainer.attr("class", "eventElemLg");
+                    $("#lgList").prepend(divContainer);
+                    $("#" + response._embedded.events[i].id).html(
+                        "<img src='" + response._embedded.events[i].images[1].url + "' style='width: 200px;'/>" +
+                        "<p>" + response._embedded.events[i].name + "</p>" +
+                        "<p>" + response._embedded.events[i]._embedded.venues[0].address.line1 + "<br />" +
+                        response._embedded.events[i]._embedded.venues[0].city.name + "</p>"
+                    );
+                }
+                console.log(response._embedded.events[0]._embedded.venues[0].city.name);
+                console.log(queryURL);
+            })
+    });
+}
