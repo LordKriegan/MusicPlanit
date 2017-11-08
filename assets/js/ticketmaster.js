bb9140ac6201b8753fde2a076c7e4e1e415f446f
unknown <you@example.com>
2017-11-07 18:53:12 -0800

    Testing picture on click

--- assets/js/ticketmaster.js
+++ assets/js/ticketmaster.js
@@ -23,14 +23,19 @@ window.onload = function() {
                     divContainer.attr("class", "eventElemLg");
                     $("#lgList").prepend(divContainer);
                     $("#" + response._embedded.events[i].id).html(
-                        "<img src='" + response._embedded.events[i].images[1].url + "' style='width: 200px;'/>" +
+                        "<img src='" + response._embedded.events[i].images[0].url + "' style='width: 200px;'/>" +
                         "<p>" + response._embedded.events[i].name + "</p>" +
                         "<p>" + response._embedded.events[i]._embedded.venues[0].address.line1 + "<br />" +
                         response._embedded.events[i]._embedded.venues[0].city.name + "</p>"
+                        //"<p>" + response._embedded.events[i].url + "</p>"
                     );
+                    //$("#eventElemLg").html(<a href= response._embedded.events[i].url><img src = "#" + response._embedded.events[i].id></a>)
                 }
                 console.log(response._embedded.events[0]._embedded.venues[0].city.name);
                 console.log(queryURL);
             })
     });
 }
+
+//Create links within image so it can popup to the page
+
